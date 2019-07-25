960030c29ce9f2f730db6b84babfa3c2f04cb803
Tanish2623 <bverma132@gmail.com>
2019-07-25 17:51:09 +0530

    Sub_array and largest sum of sub array

--- contigous_sum.c
+++ contigous_sum.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+void sub_array(int arr[],int size)
+{
+	int i,j,k,max=0,sum=0,start,end;
+         for(k=0;k<size;k++)
+   	 {
+       	 for(i=k;i<size;i++)
+       	 {
+            for(j=k;j<=i;j++)
+            {
+		printf("%d ",arr[j]);
+                sum=sum+arr[j];
+            }
+	    	printf("\n");	
+  	    //    printf("%d\n",sum);
+                if(sum>max)
+                {
+                    start=k;
+                    end=i;
+                    max=sum;
+                }
+                sum=0;
+        }
+    }
+    printf("The largest sum is %d starting with index %d and ending with %d",max,start,end);
+	
+}
 int main(int argc,const char *argv[])
 {
    int n,i,j=0,k,max=0,sum=0,start,end;
@@ -9,27 +35,6 @@ int main(int argc,const char *argv[])
 	arr[j]=atoi(argv[i]);
 	j++;
    }
-   n=argc-1;
-    for(k=0;k<n;k++)
-    {
-  //  printf(" k=%d\n",arr[k]);
-        for(i=k;i<n;i++)
-        {
-            for(j=k;j<=i;j++)
-            {
-                sum=sum+arr[j];
-            }
-  //               printf("%d\n",sum);
-                if(sum>max)
-                {
-                    start=k;
-                    end=i;
-                    max=sum;
-                }
-                sum=0;
-           
-        }
-    }
-    printf("%d %d %d",max,start,end);
+   sub_array(arr,j);
     return 0;
 }
